df72af292eef4861d5d5b93ef64f6597ef80c40b
Petar Đorđević <petar.djordjevic@matf.bg.ac.rs>
2025-02-17 23:31:41 +0100

    Revert "markdown enabled for bio and footer"
    This reverts commit 003ddfd31f9088baad87db41d03f161f2e6259c9.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,24 @@
-import { useEffect, useState } from "react";
+// src/components/Footer.js
+import React from "react";
 import { Title } from "./Title";
-import { fetchMarkdown } from "../utils/FetchMarkdown";
+import kontakt from "../data/kontakt.json";
 
 function Footer() {
-  const [content, setContent] = useState({ attributes: [] });
-
-  useEffect(() => {
-    const loadMarkdown = async () => {
-      const parsedContent = await fetchMarkdown("/sajt/data/biografija.md");
-      setContent(parsedContent);
-    };
-
-    loadMarkdown();
-  }, []);
-
-  return (
-    <footer className="bg-white text-white text-left px-[15%] py-12">
-      <Title content={"Контакт информације"} />
-      {content.attributes && content.attributes.length > 0 ? (
-        content.attributes.map((kontakt) => (
-          <div key={kontakt.key} className="mb-6">
-            <p className="text-base text-[#1E1818] mb-2">{kontakt.key}</p>
-            <p className="text-base text-[#1E1818] font-semibold">
-              {kontakt.value}
-            </p>
-          </div>
-        ))
-      ) : (
-        <p>No contact information available</p>
-      )}
-    </footer>
-  );
+	return (
+		<footer className="bg-white text-white text-left px-[15%] py-12">
+			<Title content={"Контакт информације"} />
+			{kontakt.map((kontakt) => {
+				return (
+					<div className="mb-6">
+						<p className="text-base text-[#1E1818] mb-2">{kontakt.key}</p>
+						<p className="text-base text-[#1E1818] font-semibold">
+							{kontakt.value}
+						</p>
+					</div>
+				);
+			})}
+		</footer>
+	);
 }
 
 export default Footer;
