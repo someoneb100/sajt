38a304aba5ec90a3c11d35c385233ca99f8f1384
Petar Đorđević <petar.djordjevic@matf.bg.ac.rs>
2025-02-19 21:09:45 +0100

    bio now is read from markdown

--- src/components/Biografija.js
+++ src/components/Biografija.js
@@ -1,15 +1,29 @@
+import { Buffer } from "buffer";
+import { useState, useEffect } from "react";
+import matter from "gray-matter";
 import { SinglePage } from "./SinglePage";
-import Profilna from "../shared/profilnaSlika.png"
-import Bio from "../data/biografija.json"
+import Profilna from "../shared/profilnaSlika.png";
+import Bio from "../data/biografija.md"
 
 export const Biografija = () => {
-	return (
-		<SinglePage
-			title="Biografija"
-			image={{ href: Profilna, alt: "Петар Ђорђевић" }}
-			content={Bio}
-			download={{ href: "/sajt/Petar_Djordjevic_CV.pdf", text: "Преузми радну биографију"}}
-		/>
-	)
-	
+    const [bioContent, setBioContent] = useState("");
+
+    useEffect(() => {
+		window.Buffer = window.Buffer || Buffer;
+        fetch(Bio)
+            .then((res) => res.text())
+            .then((text) => {
+                const { content, } = matter(text);
+                setBioContent(content);
+            });
+    }, []);
+
+    return (
+        <SinglePage
+            title={"Biografija"}
+            image={{ href: Profilna, alt: "Петар Ђорђевић" }}
+            content={bioContent}
+            download={{ href: "/sajt/Petar_Djordjevic_CV.pdf", text: "Преузми радну биографију" }}
+        />
+    );
 };
